4c6f14558ecc42606eea4c07a383a5b3ca2647f2
Jan Pfeifer <pfeifer@gmail.com>
2026-04-04 01:02:57 +0200

    Specialized pool not used, removed.

--- internal/pool/runtime.go
+++
@@ -1,9 +0,0 @@
-package pool
-
-import _ "unsafe"
-
-//go:linkname runtime_procPin runtime.procPin
-func runtime_procPin() int
-
-//go:linkname runtime_procUnpin runtime.procUnpin
-func runtime_procUnpin()
